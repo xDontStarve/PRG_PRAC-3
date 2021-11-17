bf4da0542f93026b9c2e4982d842bce6d20a04e1
Jialiang <jialiang.chen@estudiants.urv.cat>
2021-11-17 11:35:05 +0100

    hola

--- PRG_PRAC-3/src/dades/Llista.java
+++ PRG_PRAC-3/src/dades/Llista.java
@@ -1,5 +1,27 @@
 package dades;
 
-public class Llista {
-
+public class Llista{
+	private int nElem, mida;
+    private <?> llista;
+    private Plantes llistaPlantes[];
+    private Plantacions llistaPlantacions[];
+    private Terrenys llistaTerrenys[];
+    public Llista (int mida, Object classe) {
+        this.mida=mida;
+        nElem=0;
+        <> a = new Arrays();
+    }
+    public Llista (int mida, String nomClasse) {
+        this.mida=mida;
+        nElem=0;
+        if (nomClasse.equalsIgnoreCase("Plantes")) {
+            this.llistaPlantes = new Plantes[mida];
+        }
+        else if (nomClasse.equalsIgnoreCase("Plantacions")) {
+            this.llistaPlantacions = new Plantacions[mida];
+        }
+        else if (nomClasse.equalsIgnoreCase("Terrenys")) {
+            this.llistaTerrenys = new Terrenys[mida];
+        }
+    }
 }
