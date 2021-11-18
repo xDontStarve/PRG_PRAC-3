92cb83a8291e40a221683a9f9e9a1a84c617924d
Jose Luis Pueyo Viltres <joseluis.pueyo@estudiants.urv.cat>
2021-11-18 01:10:56 +0100

    Jose: Initial implementation of a generic array

--- PRG_PRAC-3/src/aplicacio/UsaLlistes.java
+++ PRG_PRAC-3/src/aplicacio/UsaLlistes.java
@@ -1,10 +1,23 @@
 package aplicacio;
 
+import dades.*;
+
 public class UsaLlistes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Llista<Arbres> llistaArbres = new Llista<>(3);
+		llistaArbres.afegir(new Arbres());
+		System.out.println("Llista de "+llistaArbres.getLlista()[0].getClass().getSimpleName());
+		
+		Llista<Plantes> llistaPlantes = new Llista<>(3);
+		llistaPlantes.afegir(new Plantes());
+		System.out.println("Llista de "+llistaPlantes.getLlista()[0].getClass().getSimpleName());
+		
+		Llista<Plantacions> llistaPlantacions = new Llista<>(3);
+		llistaPlantacions.afegir(new Plantacions());
+		System.out.println("Llista de "+llistaPlantacions.getLlista()[0].getClass().getSimpleName());
+		
 	}
 
 }
--- PRG_PRAC-3/src/dades/Llista.java
+++ PRG_PRAC-3/src/dades/Llista.java
@@ -1,10 +1,21 @@
 package dades;
 
-public class Llista <T>{
+public class Llista <T> {
 	private int nElem, mida;
-    private T llista[];
-    public Llista (int mida, T classe) {
+    private T[] llista;
+    
+    public Llista (int mida) {
         this.mida=mida;
         nElem=0;
+        llista = (T[]) new Object[mida];
+    }
+    
+    public boolean afegir(T element) {
+    	llista[0] = element;
+    	return true;
+    }
+    
+    public <T> T[] getLlista() {
+    	return (T[]) llista;
     }
 }
