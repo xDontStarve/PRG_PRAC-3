69f0f6a23cf647a01c7dd9b17a10f8564836a32f
Joel Lacambra <joel.lacambra@estudiants.urv.cat>
2021-12-10 20:41:15 +0100

    Joel: primera implentacio terrenys

--- PRG_PRAC-3/src/dades/Terrenys.java
+++ PRG_PRAC-3/src/dades/Terrenys.java
@@ -1,5 +1,91 @@
+/** 
+ * Classe Terrenys
+ * 
+ * @author Joel lacambra Chen
+ * @version 1.0
+ * 
+ */
 package dades;
 
-public class Terrenys {
+import java.util.Arrays;
 
+public class Terrenys {
+	private String nomTerreny;
+	private String[] plantes;
+	private int[] numPlantes;
+	
+	public Terrenys (String nomTerreny, String[] plantes, int[] numPlantes) {
+		this.nomTerreny = nomTerreny;
+		this.plantes = new String[plantes.length];
+		this.numPlantes = new int[numPlantes.length];
+		
+		for(int i=0; i<plantes.length; i++) {
+			this.plantes[i] = plantes[i];
+		}
+		for(int i=0; i<numPlantes.length; i++) {
+			this.numPlantes[i] = numPlantes[i];
+		}
+	}
+	
+	public String getNomTerreny() {
+		return nomTerreny;
+	}
+	
+	public void setNomTerreny(String nomTerreny) {
+		this.nomTerreny = nomTerreny;
+	}
+	
+	public String[] getPlantes() {
+		return plantes;
+	}
+	
+	public String getIEssimaPlanta(int i) {
+		return plantes[i];
+	}
+	
+	public void setIEssimaPlanta(String planta, int i) {
+		plantes[i] = planta;
+	}
+	
+	public int[] getNumPlantes() {
+		return numPlantes;
+	}
+	
+	public int getIEssimaNumPlanta(int i) {
+		return numPlantes[i];
+	}
+	
+	public void setIEssimaNumPlanta(int numPlanta, int i) {
+		numPlantes[i] = numPlanta;
+	}
+	
+	@Override
+	public String toString() {
+		return "nomTerreny = " + nomTerreny + ", plantes = " + Arrays.toString(plantes) + ", numPlantes = "
+				+ Arrays.toString(numPlantes);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
