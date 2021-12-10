5bc5ff5c7a79342ddcfb05d23b2ac80aad03fbca
Marc Fonseca <marc.fonseca@estudiants.urv.cat>
2021-12-10 20:40:31 +0100

    Marc: primers pasos

--- PRG_PRAC-3/src/dades/Plantacions.java
+++ PRG_PRAC-3/src/dades/Plantacions.java
@@ -1,5 +1,47 @@
+// Marc Fonseca
 package dades;
 
 public class Plantacions {
+	private String nomPartida;
+    private int anyPlantacio;
+    private LlistaRodals rodals;
+    
+    public Plantacions (String nomPartida, int anyPlantacio, LlistaRodals rodals){
+        this.nomPartida = nomPartida;
+        this.anyPlantacio = anyPlantacio;
+        this.rodals = rodals;
+       /* for (int i = 0; i < tipusTerreny.length; i++) {
+        	rodals.afegir(new Rodal(tipusTerreny[i], superficie[i]));
+        }*/
+    }
+
+	public String getNomPartida() {
+		return nomPartida;
+	}
+
+	public void setNomPartida(String nomPartida) {
+		this.nomPartida = nomPartida;
+	}
+
+	public int getAnyPlantacio() {
+		return anyPlantacio;
+	}
+
+	public void setAnyPlantacio(int anyPlantacio) {
+		this.anyPlantacio = anyPlantacio;
+	}
+
+	@Override
+	public String toString() {
+		String aux ="";
+		
+		aux = "nomPartida = " + nomPartida + ", anyPlantacio = " + anyPlantacio;
+		aux += "\t\t"+rodals.toString();
+		
+		
+		return aux;
+	}
+
+    
 
 }
